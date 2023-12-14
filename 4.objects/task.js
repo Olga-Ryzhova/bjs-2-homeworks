26ec2c54cb97eaef24119235f5c48f5f0b5663e8
Olga Ryzhova <olya_saleeva@mail.ru>
2023-12-14 16:03:32 +0400

    изменила свойство взаимодействия  в методе addMarks

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -9,9 +9,9 @@ Student.prototype.setSubject = function (subjectName) {
     this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marksToAdd) {     
+Student.prototype.addMarks = function (...marks) {     
     if (!this.excluded) {
-        this.marks.push(...marksToAdd);
+        this.marks.push(...marks);
     }      
 }
 
