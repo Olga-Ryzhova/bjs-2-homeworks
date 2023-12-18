25d8c78f9198c761edf5a547572c054efb9a2c33
Olga Ryzhova <olya_saleeva@mail.ru>
2023-12-18 21:57:44 +0400

    Решение домашнего задания по теме исключения и замыкания

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,58 @@
-﻿
+﻿// Задача 1
+
+function parseCount(number) {
+	if (Number.isNaN(Number.parseFloat(number))) {
+		throw new Error('Невалидное значение');
+	}
+	return Number.parseFloat(number);
+}
+
+function validateCount(number) {
+	try {
+		return parseCount(number);
+	} catch (error) {
+		return error;
+	}
+
+}
+
+// Задача 2
+
+class Triangle {
+	constructor(firstSide, secondSide, thirdSide) {
+		this.firstSide = firstSide;
+		this.secondSide = secondSide;
+		this.thirdSide = thirdSide;
+		if ((firstSide + secondSide < thirdSide) || (firstSide + thirdSide < secondSide) || (secondSide + thirdSide < firstSide)) {
+			throw new Error('Треугольник с такими сторонами не существует');
+		}
+	}
+
+	get perimeter() {
+		let perimetrCount = this.firstSide + this.secondSide + this.thirdSide;
+		return perimetrCount;
+	}
+
+	get area() {
+		let halfPerimetr = (this.firstSide + this.secondSide + this.thirdSide) * 0.5;
+		let areaCount = Math.sqrt(halfPerimetr * (halfPerimetr - this.firstSide) * (halfPerimetr - this.secondSide) * (halfPerimetr - this.thirdSide));
+		return +(areaCount.toFixed(3));
+	}
+}
+
+function getTriangle(firstLength, secondLength, thirdLength) {
+	try {
+		return new Triangle(firstLength, secondLength, thirdLength);
+	} catch (error) {
+		return {
+			get perimeter() {
+				return 'Ошибка! Треугольник не существует';
+			},
+			get area() {
+				return 'Ошибка! Треугольник не существует';
+			}
+		}
+	}
+}
+
+
