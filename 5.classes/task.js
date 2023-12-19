da492194fc5e9e2c933317ba9e76a1948eea99c8
Olga Ryzhova <olya_saleeva@mail.ru>
2023-12-19 11:23:37 +0400

    Доработала домашнее задание по теме Классы

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -14,9 +14,9 @@ class PrintEditionItem {
 
 	set state(newState) {
 		if (newState < 0) {
-			this.state = 0;
+			this._state = 0;
 		} else if (newState > 100) {
-			this.state = 100;
+			this._state = 100;
 		} else {
 			this._state = newState;
 		}
